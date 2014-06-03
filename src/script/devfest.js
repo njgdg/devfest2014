89c6adc352530f401e4bdae265947ff241912789
lpgray <rayzy1991@gmail.com>
2014-06-03 09:34:28 +0800

    finished home page.

---
+++ src/script/devfest.js
@@ -0,0 +1,76 @@
+(function() {
+
+  "use strict";
+
+  function loopWithPause(arr, func, time) {
+    if (!arr.length) {
+      return;
+    }
+
+    function one(idx, arr) {
+      if (idx === arr.length) {
+        return;
+      } else {
+        setTimeout(function() {
+          func.call(arr[idx], idx, arr[idx]);
+          idx++;
+          one(idx, arr);
+        }, time);
+      }
+    }
+
+    func.call(arr[0], 0, arr[0]);
+    one(1, arr);
+  }
+
+  var InputArea = function(domId) {
+    this.dom = document.getElementById(domId);
+  }
+  InputArea.prototype = {
+    run: function(wordsArr) {
+      var _ = this;
+      var i = 0;
+      _.flash = setInterval(function() {
+        _.dom.style.borderColor = (++i) % 2 === 0 ? 'transparent' : 'yellow';
+      }, 500);
+
+      var start = -1;
+      var size = wordsArr.length - 1;
+      _.input(wordsArr[++start]);
+      _.inputing = setInterval(function() {
+        _.input(wordsArr[++start]);
+        if (start === size) {
+          start = -1;
+        }
+      }, 5000);
+    },
+    input: function(word) {
+      this.dom.innerHTML = '';
+      var _ = this;
+      var chars = word.split('');
+
+      loopWithPause(chars, function(i, item) {
+        _.dom.innerHTML = _.dom.innerHTML + item;
+      }, 50);
+    }
+  }
+
+  var sloganArea = new InputArea('J_sloganArea');
+  sloganArea.run(['have fun!', 'coding!', 'rocking!']);
+
+  var map = new BMap.Map('J_map');
+  map.centerAndZoom(new BMap.Point(118.819224, 31.950503), 32);
+  var marker = new BMap.Marker(new BMap.Point(118.819224, 31.950503));
+  map.addOverlay(marker);
+  var opts = {
+    width: 220, // 信息窗口宽度      
+    height: 60, // 信息窗口高度      
+    title: "南京GDG DevFest 2014" // 信息窗口标题     
+  }
+  var infoWindow = new BMap.InfoWindow('<div style="font-size: 12px">地址：南京市鼓楼区南京大学 <br />电话：18260008278</div>', opts); // 创建信息窗口对象      
+
+  marker.addEventListener('click', function() {
+    map.openInfoWindow(infoWindow, marker.getPosition()); // 打开信息窗口
+  });
+
+}())
